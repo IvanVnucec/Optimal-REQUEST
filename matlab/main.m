2981e1c0ce8e05866a2d6d573feb4b4c5ddf9e65
Ivan Vnučec <50591550+IvanVnucec@users.noreply.github.com>
2021-03-29 18:00:11 +0200

    Update main.m

--- matlab/main.m
+++ matlab/main.m
@@ -17,10 +17,17 @@ for k = 1 : NUM_OF_ITER
     P = Phi * P * Phi.' + Q;
     
     % measurement update
+    b = [];
+    w = [];
     Rho = (m^2 * trace(P)) / (m^2 * trace(P) + dm^2 * trace(R));
     mk1 = (1 - Rho) * m + Rho * dmk1;
-    dK = []; % eq. 12, 13
+    bx = [0 -b3 b2; b3 0 -b1; -b2 b1 0];
+    zk = Rho * bx * r;
     K = (1 - Rho) * mk / mk1 * K + Rho * dmk1 / mk1 * dK;
+    B = Rho * b * r.';
+    Sigma = trace(B);
+    S = B + B.';
+    dK = 1 / Rho * [S - Sigma * eye(3), zk; zk.', Sigma]; % eq. 12, 13
     R = []; % eq. 16, 17, 44
     P = ((1 - Rho) * mk / mk1)^2 * P + (Rho * dmk1 / mk1)^2 * R;
     
