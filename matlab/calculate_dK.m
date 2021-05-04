e60fe1bedf77a3e4908caf5874f202d95ea88a5b
Ivan Vnučec <50591550+IvanVnucec@users.noreply.github.com>
2021-05-04 12:21:07 +0200

    Update calculate_dK.m

--- matlab/calculate_dK.m
+++ matlab/calculate_dK.m
@@ -1,24 +1,24 @@
-function [dK, B, S, z, Sigma] = calculate_dK(r, b, a)
+function [dK, dB, dS, dz, dSigma] = calculate_dK(r, b, a)
 %CALCULATE_DK Summary of this function goes here
 %   Detailed explanation goes here
 
 [~, ncols] = size(r);
 
 % eqs. 3.7
-B = zeros(3, 3);
-z = zeros(3, 1);
-Sigma = 0.0;
+dB = zeros(3, 3);
+dz = zeros(3, 1);
+dSigma = 0.0;
 for i = 1:ncols
     ai = a(i);
     bi = b(:,i);
     ri = r(:,i);
     
-    B = B + ai * bi * ri';
-    z = z + ai * cross(bi, ri);
-    Sigma = Sigma + ai * bi' * ri;
+    dB = dB + ai * bi * ri';
+    dz = dz + ai * cross(bi, ri);
+    dSigma = dSigma + ai * bi' * ri;
 end
-S = B + B';
+dS = dB + dB';
 
-dK = [S - Sigma * eye(3), z; z', Sigma];
+dK = [dS - dSigma * eye(3), dz; dz', dSigma];
 
 end
