e2fb0bf9c6a7cf9a7ccfe380920aeff587f1b0d9
IvanVnucec <vnucec.ivan@gmail.com>
2021-06-09 13:05:52 +0200

    optimal req separate function

--- matlab/main.m
+++ matlab/main.m
@@ -1,54 +1,17 @@
 % =========================== info ==============================
 % Script for testing the Optimal-REQUEST algorithm.
-% Works in MATLAB or GNU Octave.
+% Works in MATLAB or GNU Octave. For GNU Octave delete the
+% rng('default'); line because rng function is not yet 
+% implemented.
 %
 % Author: Ivan Vnucec, 2021
 % License: MIT
-
-% =========================== notes =============================
-% Almost all of the algorithm equations have references to some
-% paper. The reference equations are written in the form for ex.
-% Ref A eq. 25 where letter 'A' denotes reference to the paper
-% and the number '25' the equation number it that paper. The 
-% references are listed under Reference section below.
-% 
-% Indexes with k+1 are written without indexes and indexes with 
-% k are written with _k. For example dm_k+1 is written as dm and
-% dm_k is written as dmk.
-
-% ========================= references ==========================
-% Ref A: 
-%   REQUEST: A Recursive QUEST Algorithmfor Sequential Attitude Determination
-%   Itzhack Y. Bar-Itzhack, 
-%   https://sci-hub.se/https://doi.org/10.2514/3.21742
-%
-% Ref B: 
-%   Optimal-REQUEST Algorithm for Attitude Determination,
-%   D. Choukroun,I. Y. Bar-Itzhack, and Y. Oshman,
-%   https://sci-hub.se/10.2514/1.10337
-%
-% Ref. C: 
-%   Appendix B, Novel Methods for Attitude Determination Using Vector Observations,
-%   Daniel Choukroun,
-%   https://www.researchgate.net/profile/Daniel-Choukroun/publication/265455600_Novel_Methods_for_Attitude_Determination_Using_Vector_Observations/links/5509c02b0cf26198a639a83c/Novel-Methods-for-Attitude-Determination-Using-Vector-Observations.pdf#page=253
-%
-% Ref. D: 
-%   Appendixes, ttitude Determination Using Vector Observations andthe Singular Value Decomposition
-%   Markley, F. L.,
-%   http://malcolmdshuster.com/FC_Markley_1988_J_SVD_JAS_MDSscan.pdf
 
 % =========================== START =============================
 % for debug
 clear all;
 close all;
-rng('default');
-
-% =========================== constants =========================
-%dT = 0.1;              % senzor refresh time, in seconds
-%simulation_time = 72;  % in seconds
-
-%t = 0:dT:simulation_time;
-%num_of_iter = length(t);
+rng('default'); % comment this line if running on GNU Octave
 
 % ======================== measurements =========================
 % === import simulated data ===
@@ -57,7 +20,7 @@ generate_measurements
 % === white Gauss zero mean noise ===
 gyr_bdy_meas_noise_std = 0.1;       % rad/s
 acc_bdy_meas_noise_std = 0.1;       % m/s^2
-mag_bdy_meas_noise_std = 10.0;       % uT
+mag_bdy_meas_noise_std = 10.0;      % uT
 
 % === compute Mu and Eta noise variances for Q and R computation ===
 % for R computation
@@ -116,57 +79,16 @@ mk = dm0;   % Ref. B eq. 67, mk = m_k
 
 % ======================== algorithm ============================
 for k = 2 : num_of_iter
-    % =================== time update ===================
     % get angular velocity measurement
     w = gyr_bdy_meas(:,k);
     
-    % Ref. B eq. 4
-    wx = [0, -w(3), w(2); w(3), 0, -w(1); -w(2), w(1), 0];
-    
-    % Ref. B eq. 10
-    Omega = 1.0 / 2 * [-wx, w; -w', 0];
-    
-    % Ref. B eq. 9
-    Phi = expm(Omega * dT); % eq. 9
-    
-    [B, ~, z, Sigma] = get_util_matrices(K);
-    Q = calculate_Q(B, z, Sigma, Eta_noise_var, dT);
-    
-    % Ref. B eq. 11
-    K = Phi * K * Phi';
-    
-    % Ref. B eq. 69
-    P = Phi * P * Phi' + Q;
-    
-    % ================ measurement update ===============
     % referent vector measurements
     r = [mag_ref_meas(:,k), acc_ref_meas(:,k)];
     % body vector measurements
     b = [mag_bdy_meas(:,k), acc_bdy_meas(:,k)];
-    % calc. meas. weights
-    [~, ncols] = size(b); 
-    a = ones(1, ncols) ./ ncols; % equal weights 
     
-    dm = sum(a);
-    
-    R = calculate_R(r, b, Mu_noise_var);
-    
-    % Ref. B eq. 70
-    Rho = (mk^2 * trace(P)) / (mk^2 * trace(P) + dm^2 * trace(R));
-    
-    % Ref. B eq. 71
-    m = (1.0 - Rho) * mk + Rho * dm;
-    
-    dK = calculate_dK(r, b, a);
-    
-    % Ref. B eq. 72
-    K = (1.0 - Rho) * mk / m * K + Rho * dm / m * dK;
-    
-    % Ref. B eq. 73
-    P = ((1.0 - Rho) * mk / m)^2 * P + (Rho * dm / m)^2 * R;
-    
-    % for the next iteration m_k = m_k+1
-    mk = m;
+    [K, P, mk, Rho] = optimal_request(K, P, mk, w, r, b, ...
+        Mu_noise_var, Eta_noise_var, dT);
     
     % store calculated K and q for debug
     K_est(:,:,k) = K;
