48bededfd599bc027a4b857658a24c778844bca7
IvanVnucec <vnucec.ivan@gmail.com>
2021-06-09 15:47:33 +0200

    replace graphs with rms error

--- matlab/montecarlo.m
+++ matlab/montecarlo.m
@@ -7,133 +7,27 @@ colordef black
 
 Mu_noise_var_fact = 1;
 main
-figure(1)
-hold on
-plot(log(Rho_est))
-hold off
-figure(2)
-hold on
-diff = rad2deg(angle_diff(euler_gt, euler_est));
-plot(diff(1,:));
-hold off
-figure(3)
-hold on
-diff = rad2deg(angle_diff(euler_gt, euler_est));
-plot(diff(2,:));
-hold off
-figure(4)
-hold on
-diff = rad2deg(angle_diff(euler_gt, euler_est));
-plot(diff(3,:));
-hold off
-figure(5)
-hold on
-diff = rad2deg(angle_diff(angle_est, angle_gt));
-plot(diff);
-hold off
-figure(6)
-hold on
-[up, lo] = envelope(diff, 30, 'peak');
-envel = [up; lo]';
-plot(envel, 'y');
-hold off
+e = angle_diff(angle_gt, angle_est);
+s_e = e * e';
+ms_e = s_e/3;
+rms_e = sqrt(ms_e);
+fprintf('factor = %f -> rms_error = %f\n', Mu_noise_var_fact, rms_e);
 
 Mu_noise_var_fact = 0.1;
 main
-figure(1)
-hold on
-plot(log(Rho_est))
-hold off
-figure(2)
-hold on
-diff = rad2deg(angle_diff(euler_gt, euler_est));
-plot(diff(1,:));
-hold off
-figure(3)
-hold on
-diff = rad2deg(angle_diff(euler_gt, euler_est));
-plot(diff(2,:));
-hold off
-figure(4)
-hold on
-diff = rad2deg(angle_diff(euler_gt, euler_est));
-plot(diff(3,:));
-hold off
-figure(5)
-hold on
-diff = rad2deg(angle_diff(angle_est, angle_gt));
-plot(diff);
-hold off
-figure(6)
-hold on
-[up, lo] = envelope(diff, 30, 'peak');
-envel = [up; lo]';
-plot(envel, 'b');
-hold off
+e = angle_diff(angle_gt, angle_est);
+s_e = e * e';
+ms_e = s_e/3;
+rms_e = sqrt(ms_e);
+fprintf('factor = %f -> rms_error = %f\n', Mu_noise_var_fact, rms_e);
 
 Mu_noise_var_fact = 0.01;
 main
-figure(1)
-hold on
-plot(log(Rho_est))
-hold off
-figure(2)
-hold on
-diff = rad2deg(angle_diff(euler_gt, euler_est));
-plot(diff(1,:));
-hold off
-figure(3)
-hold on
-diff = rad2deg(angle_diff(euler_gt, euler_est));
-plot(diff(2,:));
-hold off
-figure(4)
-hold on
-diff = rad2deg(angle_diff(euler_gt, euler_est));
-plot(diff(3,:));
-hold off
-figure(5)
-hold on
-diff = rad2deg(angle_diff(angle_est, angle_gt));
-plot(diff);
-hold off
-figure(6)
-hold on
-[up, lo] = envelope(diff, 30, 'peak');
-envel = [up; lo]';
-plot(envel, 'r');
-hold off
-
-figure(1); 
-legend('var = var * 1', 'var = var * 0.1', 'var = var * 0.01');
-figure(2); 
-legend('var = var * 1', 'var = var * 0.1', 'var = var * 0.01');
-figure(3); 
-legend('var = var * 1', 'var = var * 0.1', 'var = var * 0.01');
-figure(4); 
-legend('var = var * 1', 'var = var * 0.1', 'var = var * 0.01');
-figure(5); 
-legend('var = var * 1', 'var = var * 0.1', 'var = var * 0.01');
-figure(6); 
-dim = [0 0 0.3 0.3];
-str = {'ZUTO var = var * 1', 'PLAVO var = var * 0.1', 'CRVENO var = var * 0.01'};
-t = annotation('textbox',dim,'String',str,'FitBoxToText','on');
-t.BackgroundColor = 'white';
-
-figure(1); 
-title('Rho (Kalmanovo pojacanje)');
-figure(2); 
-title('greska u 1. eulerovom kutu u stupnjevima');
-figure(3); 
-title('greska u 2. eulerovom kutu u stupnjevima');
-figure(4); 
-title('greska u 3. eulerovom kutu u stupnjevima');
-figure(5); 
-title('greska u kutu rotacije u stupnjevima');
-figure(6); 
-title('envelopa greske u kutu rotacije u stupnjevima');
-
-
+e = angle_diff(angle_gt, angle_est);
+s_e = e * e';
+ms_e = s_e/3;
+rms_e = sqrt(ms_e);
+fprintf('factor = %f -> rms_error = %f\n', Mu_noise_var_fact, rms_e);
 
 
 
