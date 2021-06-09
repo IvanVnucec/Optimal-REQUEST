f34b731d73e22a8f7862b6edfa2b53e00791883e
IvanVnucec <vnucec.ivan@gmail.com>
2021-06-09 16:20:00 +0200

    bugfix root mean square algorithm

--- matlab/montecarlo.m
+++ matlab/montecarlo.m
@@ -7,27 +7,24 @@ colordef black
 
 Mu_noise_var_fact = 1;
 main
-e = angle_diff(angle_gt, angle_est);
-s_e = e * e';
-ms_e = s_e/3;
+e = angle_diff(euler_est, euler_gt).^2;
+ms_e = sum(e, 2)/length(e);
 rms_e = sqrt(ms_e);
-fprintf('factor = %f -> rms_error = %f\n', Mu_noise_var_fact, rms_e);
+fprintf('factor = %f -> rms_error = [%f %f %f] rad\n', Mu_noise_var_fact, rms_e);
 
 Mu_noise_var_fact = 0.1;
 main
-e = angle_diff(angle_gt, angle_est);
-s_e = e * e';
-ms_e = s_e/3;
+e = angle_diff(euler_est, euler_gt).^2;
+ms_e = sum(e, 2)/length(e);
 rms_e = sqrt(ms_e);
-fprintf('factor = %f -> rms_error = %f\n', Mu_noise_var_fact, rms_e);
+fprintf('factor = %f -> rms_error = [%f %f %f] rad\n', Mu_noise_var_fact, rms_e);
 
 Mu_noise_var_fact = 0.01;
 main
-e = angle_diff(angle_gt, angle_est);
-s_e = e * e';
-ms_e = s_e/3;
+e = angle_diff(euler_est, euler_gt).^2;
+ms_e = sum(e, 2)/length(e);
 rms_e = sqrt(ms_e);
-fprintf('factor = %f -> rms_error = %f\n', Mu_noise_var_fact, rms_e);
+fprintf('factor = %f -> rms_error = [%f %f %f] rad\n', Mu_noise_var_fact, rms_e);
 
 
 
