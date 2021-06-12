8f0e867ab8d8c21cec8b52b754fe7703aa2e0a5c
IvanVnucec <vnucec.ivan@gmail.com>
2021-06-12 11:51:42 +0200

    reduce scope of the functions

--- matlab/optimal_request.m
+++ matlab/optimal_request.m
@@ -89,3 +89,17 @@ mk = m;
 
 end
 
+
+function [B, S, z, Sigma] = get_util_matrices(K)
+%GET_UTIL_MATRICES Returns submatrices 'B', 'S', 'z' and 'Sigma' of 'K' 
+%   matrice.
+%   The submatrices are used for the calculation of Q matrice.
+%
+%   Ref. B eq. 6 and 7, (for references list see main.m file under reference 
+%   comment section).
+
+Sigma = K(4,4);
+S = K(1:3,1:3) + Sigma * eye(3);
+B = 0.5 * S;
+z = K(1:3,4);
+end
