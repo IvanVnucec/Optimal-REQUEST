e8daddcfe1bd6270d181f1df9154c7bdf6aacb6c
IvanVnucec <vnucec.ivan@gmail.com>
2021-06-12 11:50:10 +0200

    montecarlo file code refactoring

--- matlab/montecarlo.m
+++ matlab/montecarlo.m
@@ -9,36 +9,35 @@ clear all;
 colordef black
 
 generate_measurements
+
+% save measurement noise variance for later use
 Mu_noise_var_const = Mu_noise_var;
 
 hold on
 
+% test with the same measurement variance
 Mu_noise_var_fact = 1;
 Mu_noise_var = Mu_noise_var_const * Mu_noise_var_fact;
 main
-e = angle_diff(euler_est, euler_gt).^2;
-% calculate rms of errors in every iteration of optimal req
-rms_err_by_iter = sqrt(mean(e.^2));
+rms_err = euler_rms_error(euler_est, euler_gt);
 disp_name = sprintf('Fact = %f', Mu_noise_var_fact);
-plot(rms_err_by_iter', 'DisplayName', disp_name)
+plot(rms_err', 'DisplayName', disp_name)
 
+% test with the 10 times smaller measurement variance
 Mu_noise_var_fact = 0.1;
 Mu_noise_var = Mu_noise_var_const * Mu_noise_var_fact;
 main
-e = angle_diff(euler_est, euler_gt).^2;
-% calculate rms of errors in every iteration of optimal req
-rms_err_by_iter = sqrt(mean(e.^2));
+rms_err = euler_rms_error(euler_est, euler_gt);
 disp_name = sprintf('Fact = %f', Mu_noise_var_fact);
-plot(rms_err_by_iter', 'DisplayName', disp_name)
+plot(rms_err', 'DisplayName', disp_name)
 
+% test with the 100 times smaller measurement variance
 Mu_noise_var_fact = 0.01;
 Mu_noise_var = Mu_noise_var_const * Mu_noise_var_fact;
 main
-e = angle_diff(euler_est, euler_gt).^2;
-% calculate rms of errors in every iteration of optimal req
-rms_err_by_iter = sqrt(mean(e.^2));
+rms_err = euler_rms_error(euler_est, euler_gt);
 disp_name = sprintf('Fact = %f', Mu_noise_var_fact);
-plot(rms_err_by_iter', 'DisplayName', disp_name)
+plot(rms_err', 'DisplayName', disp_name)
 
 hold off
 
@@ -46,9 +45,8 @@ legend;
 xlabel('k');
 ylabel('RMS error of all 3 angles');
 
-
-
-
-
-
-
+% Calculate Euler angles RMS error between 3 Euler angles
+function rms_err = euler_rms_error(euler_est, euler_gt)
+    e = angle_diff(euler_est, euler_gt).^2;
+    rms_err = sqrt(mean(e.^2));
+end
