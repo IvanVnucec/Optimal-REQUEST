136cff6869102a9f0fe71c64e1b1c17cca194fb1
IvanVnucec <vnucec.ivan@gmail.com>
2021-07-02 22:41:35 +0200

    delete unused private function

--- matlab/get_quat_from_K.m
+++ matlab/get_quat_from_K.m
@@ -5,6 +5,11 @@ function [q] = get_quat_from_K(K)
 %
 %   Ref. B states:
 %   The optimal quaternionË†qk+1/k+1is the eigenvector ofKk+1/k+1,which belongs to its maximal eigenvalue.
+%
+% Author:     Ivan Vnucec
+% University: FER, Zagreb
+% Date:       May, 2021
+% License:    MIT
 
 % get eigenvector
 [V, D] = eig(K);
